d52af500bb35ad5e6791f2dd088b8c24cae2e57e
ThomasCode92 <thomascode92@hotmail.com>
2023-03-08 12:51:40 +0100

    Working with Multiple State Properties

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import { legacy_createStore as createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const INITIAL_STATE = { counter: 0, showCounter: true };
+
+const counterReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'INCREMENT': {
-      return { counter: state.counter + 1 };
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
     }
     case 'DECREMENT': {
-      return { counter: state.counter - 1 };
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
     }
     case 'INCREASE': {
-      return { counter: state.counter + action.payload };
+      return {
+        counter: state.counter + action.payload,
+        showCounter: state.showCounter,
+      };
+    }
+    case 'TOGGLE': {
+      return {
+        counter: state.counter,
+        showCounter: !state.showCounter,
+      };
     }
     default: {
       return state;
