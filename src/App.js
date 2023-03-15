51cb4915dfaabd2cdc712a8fbeeb3b772dad120e
ThomasCode92 <thomascode92@hotmail.com>
2023-03-15 13:10:17 +0100

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { uiActions } from './components/store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 
+let isInitial = true;
 const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const ui = useSelector(state => state.ui);
   const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+  const { cartIsVisible: showCart, notification } = ui;
 
   useEffect(() => {
-    fetch(FIREBASE_URL + '/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const response = await fetch(FIREBASE_URL + '/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
     });
-  }, [cart]);
+  }, [dispatch, cart]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
