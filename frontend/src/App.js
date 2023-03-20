b0bcd2e2056755ca8ed9674f61d9326baeedcf92
ThomasCode92 <thomascode92@hotmail.com>
2023-03-20 12:52:19 +0100

    Add Routing & Route Definitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
 // Challenge / Exercise
 
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
@@ -15,9 +9,24 @@
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+import EventsPage from './pages/Events';
+import EventDetailPage from './pages/EventDetail';
+import EditEventPage from './pages/EditEvent';
+import NewEventPage from './pages/NewEvent';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/events', element: <EventsPage /> },
+  { path: '/events/:eventId', element: <EventDetailPage /> },
+  { path: '/events/:eventId/edit', element: <EditEventPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
