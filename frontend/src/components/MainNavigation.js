c0e042dcf04211ba360ca53dc1eb20fa4908b502
ThomasCode92 <thomascode92@hotmail.com>
2023-03-22 14:20:45 +0100

    Add Working Links to MainNavigation

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
@@ -7,10 +9,10 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <a>Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a>Events</a>
+            <Link to="/events">Events</Link>
           </li>
         </ul>
       </nav>
