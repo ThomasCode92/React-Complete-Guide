7060cd4f3f0ddc6b16dd1d68423ed2b543389111
ThomasCode92 <thomascode92@hotmail.com>
2023-03-23 12:48:32 +0100

    Output (Dummy) Events on Events Page

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const DUMMY_EVENTS = [
+  { id: 'e1', title: 'Some Event' },
+  { id: 'e2', title: 'Another Event' },
+];
 
 function EventsPage() {
-  return <h1>Events</h1>;
+  return (
+    <Fragment>
+      <h1>Events</h1>
+      <ul>
+        {DUMMY_EVENTS.map(event => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
 }
 
 export default EventsPage;
