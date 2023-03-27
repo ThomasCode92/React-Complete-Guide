557a67cbc227ec26c017686d42eecbacd003c2c0
ThomasCode92 <thomascode92@hotmail.com>
2023-03-27 13:24:16 +0200

    Quick Cleanup Work

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from 'react';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-  const navigation = useNavigation();
-
   return (
     <Fragment>
       <MainNavigation />
       <main>
-        {navigation.state === 'loading' && <p>Loading...</p>}
         <Outlet />
       </main>
     </Fragment>
