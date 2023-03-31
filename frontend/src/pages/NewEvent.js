93f9f3efcf2b8e707617de56b54dc07330334d52
ThomasCode92 <thomascode92@hotmail.com>
2023-03-31 17:36:22 +0200

    Planning Data Submission

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import EventForm from '../components/EventForm';
 
 function NewEventPage() {
-  return <h1>New Event</h1>;
+  return <EventForm />;
 }
 
 export default NewEventPage;
