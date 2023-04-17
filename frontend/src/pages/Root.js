32d6ee8c171ffaa062e8deba895533e209d4e59f
ThomasCode92 <thomascode92@hotmail.com>
2023-04-17 07:41:38 +0200

    Adding Automatic Logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,20 @@
-import React, { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) return;
+
+    setTimeout(() => {
+      submit(null, { action: '/auth/logout', method: 'post' });
+    }, 60 * 60 * 1000);
+  }, [submit, token]);
+
   return (
     <Fragment>
       <MainNavigation />
