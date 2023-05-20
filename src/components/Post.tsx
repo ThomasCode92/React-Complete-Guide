70663b9fac83200a6c308a11c77984671522b6ff
ThomasCode92 <thomascode92@hotmail.com>
2023-05-20 19:31:41 +0200

    Outputting Dynamic Values

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+const names = ['Thomas', 'Maximilian'];
+
 function Post() {
+  const randomName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Thomas</p>
+      <p>{randomName}</p>
       <p>React.js is awesome!</p>
     </div>
   );
