ae7d47cca499755f66fa1f3b15124d4154692354
ThomasCode92 <thomascode92@hotmail.com>
2023-05-23 19:11:32 +0200

    Preparing the App for State Management

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manual" body="Check out the full course!" />
-    </ul>
+    <Fragment>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author="Manual" body="Check out the full course!" />
+      </ul>
+    </Fragment>
   );
 }
 
