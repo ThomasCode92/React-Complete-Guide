b2889404a11e46adacbf77532cd8c6857f5c4e5d
ThomasCode92 <thomascode92@hotmail.com>
2023-05-29 19:57:51 +0200

    Lifting State Up

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,15 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { ChangeEvent, Fragment, useState } from 'react';
 
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  const bodyChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
     <Fragment>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manual" body="Check out the full course!" />
       </ul>
     </Fragment>
