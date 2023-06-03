5b04d55ff343037cb14c003efa579cf65af46563
ThomasCode92 <thomascode92@hotmail.com>
2023-06-03 12:47:37 +0200

    Outputting List Data

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -31,10 +31,19 @@ function PostsList({ modalIsVisible, onCloseModal }: PostsListProps) {
           <NewPost onCancel={closeModalHandler} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <PostItem author="Max" body="Learn React.js" />
-        <PostItem author="Manual" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map(post => (
+            <PostItem key={post.id} body={post.body} author={post.author} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </Fragment>
   );
 }
