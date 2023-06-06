d11d1e94db1b406590c6e3fa4d2f43a5f21d46b6
ThomasCode92 <thomascode92@hotmail.com>
2023-06-06 20:43:57 +0200

    Working with Layout Routes

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from '../App';
+import RootLayout from './RootLayout';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
   {
-    path: 'create-post',
-    element: <h1>Create Post</h1>,
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <App /> },
+      { path: 'create-post', element: <h1>Create Post</h1> },
+    ],
   },
 ]);
 
