29388e6716ccaa6eabb20cc030f6a87b1e97c92f
ThomasCode92 <thomascode92@hotmail.com>
2023-09-13 07:45:28 +0200

    Understanding & Configuring Query Behaviors

--- apps/frontend/src/components/Events/NewEventsSection.jsx
+++ apps/frontend/src/components/Events/NewEventsSection.jsx
@@ -10,6 +10,8 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
+    staleTime: 5000,
+    // gc: 1000,
   });
 
   let content;
