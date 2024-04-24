e000748e63d58485eade8e2e839b3582e422119e
Thomas <thomascode92@hotmail.com>
2024-04-24 07:53:05 +0200

    Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Header';
+import Results from './components/Results';
 import UserInput from './components/UserInput';
 
+const INITIAL_USER_INPUT = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
 export default function App() {
+  const [userInput, setUserInput] = useState(INITIAL_USER_INPUT);
+
+  function handleChange(inputIdentifier, value) {
+    setUserInput(prevUserInput => ({
+      ...prevUserInput,
+      [inputIdentifier]: value,
+    }));
+  }
+
   return (
     <Fragment>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </Fragment>
   );
 }
