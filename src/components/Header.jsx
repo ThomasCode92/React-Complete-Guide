50919179f24f0d87b2d8fa7be550b56968cf16f4
Thomas <thomascode92@hotmail.com>
2024-05-07 19:02:44 +0200

    Adding & Using Tailwind CSS In A React Project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
         alt="A canvas"
         className="mb-8 h-44 w-44 object-contain"
       />
-      <h1 className="text-center text-4xl font-semibold uppercase tracking-widest text-amber-800">
+      <h1 className="text-center font-title text-4xl font-semibold uppercase tracking-widest text-amber-800">
         ReactArt
       </h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
