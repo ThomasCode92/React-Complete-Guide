ce5254e90abe60fb7f0a1e4dd3ee88d7720c13b1
Thomas <thomascode92@hotmail.com>
2024-05-28 18:21:46 +0200

    Styling Buttons & Inputs with TailwindCSS

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,18 @@
+import tw from '../utils/tailwind-template';
+
 export default function Input({ label, textarea, ...props }) {
+  const classes = tw`w-full rounded-sm border-b-2 border-stone-300 bg-stone-200 p-1 text-stone-600 focus:border-stone-600 focus:outline-none`;
+
   return (
-    <p>
-      <label>{label}</label>
-      {textarea ? <textarea {...props}></textarea> : <input {...props} />}
+    <p className="my-4 flex flex-col gap-1">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea className={classes} {...props}></textarea>
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </p>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,11 +2,17 @@ import Input from './Input';
 
 export default function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="mt-16 w-[35rem]">
+      <menu className="my-4 flex items-center justify-end gap-4">
         <li>
-          <button>Cancel</button>
-          <button>Save</button>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button className="rounded-md bg-stone-800 px-6 py-2 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
         </li>
       </menu>
       <div>
