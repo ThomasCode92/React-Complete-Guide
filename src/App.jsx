0c7b5d68f82ebbebbacc06ac51ed9743517e80e3
Thomas <thomascode92@hotmail.com>
2024-05-28 18:09:45 +0200

    Adding the "New Project" Component & A Reusable "Input" Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from './components/NewProject';
 import ProjectsSidebar from './components/ProjectsSidebar';
 
 export default function App() {
   return (
-    <main className="my-8 h-screen">
+    <main className="my-8 flex h-screen gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
