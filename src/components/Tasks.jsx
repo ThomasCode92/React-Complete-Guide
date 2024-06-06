125401010713808e3d3306f3e868872e6bf2f24e
Thomas <thomascode92@hotmail.com>
2024-06-06 20:19:10 +0200

    Adding "Project Tasks" & A Tasks Component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+import NewTask from './NewTask';
+
+export default function Tasks() {
+  return (
+    <section>
+      <h2 className="mb-4 text-2xl font-bold text-stone-700">Tasks</h2>
+      <NewTask />
+      <p className="my-4 text-stone-800">
+        This project does not have any tasks yet.
+      </p>
+      <ul></ul>
+    </section>
+  );
+}
