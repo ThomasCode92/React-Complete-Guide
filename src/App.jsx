ec6dfabe413e4430318756928783c9e41e6e028c
Thomas <thomascode92@hotmail.com>
2024-06-14 07:52:13 +0200

    Linking the Context to State

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,13 @@ export default function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -78,7 +83,7 @@ export default function App() {
       <Shop>
         {DUMMY_PRODUCTS.map(product => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
