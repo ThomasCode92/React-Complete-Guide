0828c81f0dd0f4b4736e293c65fdd6739c749368
Thomas <thomascode92@hotmail.com>
2024-08-04 09:15:31 +0200

    Creating Flexible Custom Hooks

--- frontend/src/components/AvailablePlaces.jsx
+++ frontend/src/components/AvailablePlaces.jsx
@@ -3,26 +3,30 @@ import Places from './Places.jsx';
 
 import { useFetch } from '../hooks/useFetch.js';
 import { fetchAvailablePlaces } from '../http.js';
-// import { sortPlacesByDistance } from '../loc.js';
+import { sortPlacesByDistance } from '../loc.js';
 
-// navigator.geolocation.getCurrentPosition(position => {
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude,
-//   );
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
 
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude,
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const {
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
     isFetching,
     error,
-  } = useFetch(fetchAvailablePlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error)
     return <Error title="An error occurred!" message={error.message} />;
