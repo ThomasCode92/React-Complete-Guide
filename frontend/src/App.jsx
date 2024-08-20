505dabdf1e576b5da89cdd38e3ef8f4173a04f66
Thomas <thomascode92@hotmail.com>
2024-08-20 21:51:06 +0200

    Adding a First Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
+import Header from './components/Header';
+
 export default function App() {
   return (
     <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>
-        Don&apos;t worry - we&apos;ve all been there. Let&apos;s build it
-        together!
-      </p>
+      <Header />
     </>
   );
 }
