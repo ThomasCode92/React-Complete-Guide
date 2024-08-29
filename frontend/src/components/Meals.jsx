89ebae7deeb5c0140f3a52381c0be2e7cd94f59d
Thomas <thomascode92@hotmail.com>
2024-08-29 19:58:36 +0200

    Adding a Custom HTTP Hook & Avoiding Common Errors

--- frontend/src/components/Meals.jsx
+++ frontend/src/components/Meals.jsx
@@ -1,31 +1,15 @@
-import { useEffect, useState } from 'react';
-
 import MealItem from './MealItem';
 
+import useHttp from '../hooks/useHttp';
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const { data, isLoading } = useHttp('http://localhost:3000/meals');
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
-
-      if (!response.ok) {
-        // ...
-      }
-
-      return await response.json();
-    }
-
-    fetchMeals().then(meals => {
-      setLoadedMeals(meals);
-    });
-  }, []);
+  if (isLoading) return <p>Fetching meals...</p>;
 
   return (
     <ul id="meals">
-      {loadedMeals.map(meal => (
-        <MealItem key={meal.id} meal={meal} />
-      ))}
+      {data && data.map(meal => <MealItem key={meal.id} meal={meal} />)}
     </ul>
   );
 }
