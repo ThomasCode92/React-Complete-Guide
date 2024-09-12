f95e23369e0a4709d26d5c2398b79000a7d34d86
Thomas <thomascode92@hotmail.com>
2024-09-12 12:17:50 +0200

    Diving into Redux (An Alternative to the Context API)

---
+++ complete-path/redux-introduction/src/App.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import Auth from './components/Auth';
+import Counter from './components/Counter';
+import Header from './components/Header';
+
+function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <Counter />}
+    </Fragment>
+  );
+}
+
+export default App;
