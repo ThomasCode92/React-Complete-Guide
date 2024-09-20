fca3409eed2411174296f8e7955e3f1270fb00d1
Thomas <thomascode92@hotmail.com>
2024-09-20 17:34:11 +0200

    Outputting Meals Data

--- complete-path/nextjs-introduction/app/meals/page.js
+++ complete-path/nextjs-introduction/app/meals/page.js
@@ -1,7 +1,27 @@
+import { Fragment } from "react";
+
+import MealsGrid from "@/components/meals/MealsGrid";
+import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <Fragment>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </Fragment>
   );
 }
