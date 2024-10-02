409e9925889596c483f03dd6869e077433159725
Thomas <thomascode92@hotmail.com>
2024-10-02 08:30:28 +0200

    Adding Dynamic Metadata

--- complete-path/nextjs-introduction/app/meals/[mealSlug]/page.js
+++ complete-path/nextjs-introduction/app/meals/[mealSlug]/page.js
@@ -6,6 +6,17 @@ import { getMealBySlug } from "@/lib/meals";
 import { notFound } from "next/navigation";
 import styles from "./page.module.css";
 
+export async function generateMetadata({ params }) {
+  const meal = getMealBySlug(params.mealSlug);
+
+  if (!meal) notFound();
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMealBySlug(params.mealSlug);
 
