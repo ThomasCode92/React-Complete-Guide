9a65f95b279da2cede7f7b314daa58d7a368c621
Thomas <thomascode92@hotmail.com>
2024-10-06 16:05:51 +0200

    Introducing Compound Components

--- complete-path/patterns-best-practices/src/App.jsx
+++ complete-path/patterns-best-practices/src/App.jsx
@@ -1,5 +1,38 @@
-function App() {
-  return <h1>React Patterns & Practices</h1>;
-}
+import Accordion from './components/accordion/Accordion';
+import AccordionItem from './components/accordion/AccordionItem';
 
-export default App;
+export default function App() {
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            title="We got 20 years of experience"
+            className="accordion-item"
+          >
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in business of planning highly individualized vacation
+                trips for more than 20 years.
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            title="We're working with local guides"
+            className="accordion-item"
+          >
+            <article>
+              <p>We are not doing this along from our office.</p>
+              <p>
+                Instead we are working with local guides to ensure a safe and
+                pleasant vacation.
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
+}
